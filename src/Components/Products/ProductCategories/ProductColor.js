5834282daec2f997cf2579e67bb87bcacf127d1a
Tajinboxx <TajLonghurstsoft@gmail.com>
2022-03-17 00:04:46 +1300

    Added Color to the filters on Products Page
    finaly

--- src/Components/Products/ProductCategories/ProductColor.js
+++ src/Components/Products/ProductCategories/ProductColor.js
@@ -6,14 +6,86 @@ const ProductColor = () => {
     <div className={classes.colorconatiner}>
       <h4 className={classes.colorheader}>COLOR</h4>
       <ul className={classes.colorlist}>
-        <li className={classes.coloritem}>Red</li>
-        <li className={classes.coloritem}>Orange</li>
-        <li className={classes.coloritem}>Green</li>
-        <li className={classes.coloritem}>Blue</li>
-        <li className={classes.coloritem}>Purple</li>
-        <li className={classes.coloritem}>DarkPurple</li>
-        <li className={classes.coloritem}>Black</li>
-        <li className={classes.coloritem}>White</li>
+        <li className={classes.coloritem}>
+          <div className={classes.colorbox}>
+            <label className={classes.containerbox}>
+              <input type="checkbox" />
+              <span
+                className={`${classes.checkmark} ${classes.checkmarkRed}`}
+              ></span>
+            </label>
+          </div>
+        </li>
+        <li className={classes.coloritem}>
+          <div className={classes.colorbox}>
+            <label className={classes.containerbox}>
+              <input type="checkbox" />
+              <span
+                className={`${classes.checkmark} ${classes.checkmarkOrange}`}
+              ></span>
+            </label>
+          </div>
+        </li>
+        <li className={classes.coloritem}>
+          <div className={classes.colorbox}>
+            <label className={classes.containerbox}>
+              <input type="checkbox" />
+              <span
+                className={`${classes.checkmark} ${classes.checkmarkGreen}`}
+              ></span>
+            </label>
+          </div>
+        </li>
+        <li className={classes.coloritem}>
+          <div className={classes.colorbox}>
+            <label className={classes.containerbox}>
+              <input type="checkbox" />
+              <span
+                className={`${classes.checkmark} ${classes.checkmarkBlue}`}
+              ></span>
+            </label>
+          </div>
+        </li>
+        <li className={classes.coloritem}>
+          <div className={classes.colorbox}>
+            <label className={classes.containerbox}>
+              <input type="checkbox" />
+              <span
+                className={`${classes.checkmark} ${classes.checkmarkPurple}`}
+              ></span>
+            </label>
+          </div>
+        </li>
+        <li className={classes.coloritem}>
+          <div className={classes.colorbox}>
+            <label className={classes.containerbox}>
+              <input type="checkbox" />
+              <span
+                className={`${classes.checkmark} ${classes.checkmarkDarkPurple}`}
+              ></span>
+            </label>
+          </div>
+        </li>
+        <li className={classes.coloritem}>
+          <div className={classes.colorbox}>
+            <label className={classes.containerbox}>
+              <input type="checkbox" />
+              <span
+                className={`${classes.checkmark} ${classes.checkmarkBlack}`}
+              ></span>
+            </label>
+          </div>
+        </li>
+        <li className={classes.coloritem}>
+          <div className={classes.colorbox}>
+            <label className={classes.containerbox}>
+              <input type="checkbox" />
+              <span
+                className={`${classes.checkmarkWhitedot} ${classes.checkmarkWhite}`}
+              ></span>
+            </label>
+          </div>
+        </li>
       </ul>
     </div>
   );
