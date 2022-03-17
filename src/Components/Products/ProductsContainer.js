d76d3a3d7d07ed27041b2973d19baee447eb050b
Tajinboxx <TajLonghurstsoft@gmail.com>
2022-03-17 23:12:01 +1300

    Small Filter on & off btn added
    using useState. will need to change to Redux state

--- src/Components/Products/ProductsContainer.js
+++ src/Components/Products/ProductsContainer.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import ProductsFilter from "./ProductsFilter";
 import ProductList from "./ProductList";
 import ModalMobileFilter from "../Modals/FilterModal/ModalMobileFilter";
 import useWindowSize from "../../Hooks/use-windowSize";
 import classes from "./ProductsContainer.module.css";
+import filterIcon from "../../Assets/Icons/bx-filter.svg";
 import "../../index.css";
 
 const ProductsContainer = () => {
   const { isMobileView: mobile } = useWindowSize();
+  const [filterIsClicked, setFilterIsClicked] = useState(false);
+
+  const onClickMobileFilterBtn = () => {
+    setFilterIsClicked((prevState) => !prevState);
+  };
+
   return (
     <div className={classes.container}>
+      {mobile && (
+        <button
+          onClick={onClickMobileFilterBtn}
+          type="button"
+          className={classes.mobileFilterBtn}
+        >
+          <img
+            className={classes.filterIcon}
+            src={filterIcon}
+            alt="Img failed"
+          />
+        </button>
+      )}
+      {filterIsClicked && <ModalMobileFilter />}
       {!mobile && <ProductsFilter />}
-      {mobile && <ModalMobileFilter />}
       <ProductList />
     </div>
   );
