af65aa5420e7cb7ba12891ad79d79846f16439b6
Tajinboxx <TajLonghurstsoft@gmail.com>
2022-03-21 22:33:14 +1300

    added small info

--- src/Components/IndividualDetails/ProductInfo.js
+++ src/Components/IndividualDetails/ProductInfo.js
@@ -1,7 +1,27 @@
 import React from "react";
+import classes from "../IndividualDetails/ProductInfo.module.css";
 
 const ProductInfo = () => {
-  return <div>ProductInfo</div>;
+  return (
+    <div className={classes.container}>
+      <h6 className={classes.categories}>T-Shirt</h6>
+      <h1 className={classes.header}>Blue Shirt</h1>
+      <ul className={classes.sizes}>
+        <li className={classes.sizesBody}>
+          <p className={classes.sizeText}>XL</p>
+        </li>
+        <li className={classes.sizesBody}>
+          <p className={classes.sizeText}>L</p>
+        </li>
+        <li className={classes.sizesBody}>
+          <p className={classes.sizeText}>M</p>
+        </li>
+        <li className={classes.sizesBody}>
+          <p className={classes.sizeText}>S</p>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default ProductInfo;
