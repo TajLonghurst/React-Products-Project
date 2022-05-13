4508023d7ae53962e9814ab4822a1d2b3aed22a3
Tajinboxx <TajLonghurstsoft@gmail.com>
2022-05-13 23:24:34 +1200

    Added HomePage as Mobile Friendly

--- src/Components/Home/HomeNavigation.js
+++ src/Components/Home/HomeNavigation.js
@@ -9,10 +9,12 @@ import ShirtHand from "../../Assets/Images/ShirtHand.png";
 import ShirtBlue from "../../Assets/Images/ShirtBlue.png";
 import { motion } from "framer-motion";
 import { navLinkdisapear } from "../../Animations/Home-Animations";
+import useWindowSize from "../../Hooks/use-windowSize";
 
 const HomeNavigation = () => {
   const [isHover, setIsHover] = useState(false);
   const [shirtType, setShirtType] = useState(null);
+  const { isMobileView } = useWindowSize();
 
   const onMouseHoverProducts = () => {
     setIsHover((prev) => !prev);
@@ -24,47 +26,53 @@ const HomeNavigation = () => {
     setShirtType(ShirtBlue);
   };
 
+  const computerLink = (
+    <div className={classes.nav}>
+      <motion.div
+        className={classes.navitem}
+        variants={navLinkdisapear}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+      >
+        <NavLink
+          to="/products"
+          onMouseEnter={onMouseHoverProducts}
+          onMouseLeave={onMouseHoverProducts}
+          className={classes.navlink}
+        >
+          Products
+        </NavLink>
+      </motion.div>
+      <HomeMainLogo switchClass={isHover} />
+      <motion.div
+        className={classes.navitem}
+        variants={navLinkdisapear}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+      >
+        <NavLink
+          to="/admin"
+          onMouseEnter={onMouseHoverAdmin}
+          onMouseLeave={onMouseHoverAdmin}
+          className={classes.navlink}
+        >
+          Admin
+        </NavLink>
+      </motion.div>
+    </div>
+  );
+
+  const mobileLinks = <HomeMainLogo switchClass={isHover} />;
+
+  const displayHomePageLink = !isMobileView ? computerLink : mobileLinks;
+
   return (
     <Fragment>
       <HomeCart />
       <HomeMainImage isHover={isHover} shirtType={shirtType} />
-      <div className={classes.container}>
-        <div className={classes.nav}>
-          <motion.div
-            className={classes.navitem}
-            variants={navLinkdisapear}
-            initial="hidden"
-            animate="visible"
-            exit="exit"
-          >
-            <NavLink
-              to="/products"
-              onMouseEnter={onMouseHoverProducts}
-              onMouseLeave={onMouseHoverProducts}
-              className={classes.navlink}
-            >
-              Products
-            </NavLink>
-          </motion.div>
-          <HomeMainLogo switchClass={isHover} />
-          <motion.div
-            className={classes.navitem}
-            variants={navLinkdisapear}
-            initial="hidden"
-            animate="visible"
-            exit="exit"
-          >
-            <NavLink
-              to="/admin"
-              onMouseEnter={onMouseHoverAdmin}
-              onMouseLeave={onMouseHoverAdmin}
-              className={classes.navlink}
-            >
-              Admin
-            </NavLink>
-          </motion.div>
-        </div>
-      </div>
+      <div className={classes.container}>{displayHomePageLink}</div>
     </Fragment>
   );
 };
